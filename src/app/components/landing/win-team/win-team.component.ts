11c6fd98f91763d21ddbb259c394ca173e74114d
herui912 <herui912@hotmail.com>
2020-11-22 19:14:22 -0800

    2 files modified

--- src/app/components/landing/win-team/win-team.component.ts
+++ src/app/components/landing/win-team/win-team.component.ts
@@ -6,6 +6,10 @@ import {
   SimpleChanges
 } from "@angular/core";
 
+const TEAM_DEC_DEFAULT =
+  "Team Liquis is a world renowed professional gaming organization established in 2000. Since their grassroots beginings as a Battle.net clan and StarCraft community site, they have grown into a multifaceted global company with unparalleled reach in industry.";
+const TEAM_IMG_DEFAULT = "";
+
 @Component({
   selector: "app-win-team",
   templateUrl: "./win-team.component.html",
@@ -13,6 +17,9 @@ import {
 })
 export class WinTeamComponent implements OnInit, OnChanges {
   @Input() winnerTeam: any;
+
+  teamDescDefault: string = TEAM_DEC_DEFAULT;
+  teamImgDefault: string = TEAM_IMG_DEFAULT;
 
   constructor() {}
 
